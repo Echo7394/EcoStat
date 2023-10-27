9126da044ec7b586694f9c4fbe7ee18a394cc384
Echo7394 <echo7394@protonmail.com>
2023-10-27 00:21:59 -0500

    Added fan status and functions for a physical fan button

--- EcoStat_Main/EcoStat_Main.ino
+++ EcoStat_Main/EcoStat_Main.ino
@@ -1,3 +1,12 @@
+/**************************************************************************************
+░█▀▀░█▀▀░█▀█░█▀▀░▀█▀░█▀█░▀█▀     													  
+░█▀▀░█░░░█░█░▀▀█░░█░░█▀█░░█░                                                          
+░▀▀▀░▀▀▀░▀▀▀░▀▀▀░░▀░░▀░▀░░▀░                                                          
+GNU GENERAL PUBLIC LICENSE V3.0                                                       
+Made by Echo7394                                                                      
+                                                                                      
+Added variables for mode button, and a cooling relay, not implemented yet.           
+**************************************************************************************/
 #include <Wire.h>
 #include <DHT.h>
 #include <Adafruit_SSD1306.h>
@@ -12,14 +21,17 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define DHT_PIN 8    // Settings the data pin for the DHT22
+#define DHT_PIN 8    // Sets the data pin for the DHT22
 DHT dht(DHT_PIN, DHT22);
 
+#define BUTTON_FAN 5 // Button to switch fan on and off manually
+#define BUTTON_MODE 4 // Button to select "Heating / Cooling / Off"
 #define BUTTON_PIN0 3 // Button to lower tempSet
 #define BUTTON_PIN1 2 // Button to raise tempSet
 
-#define RELAY_PIN 10 // Establishing the trigger pin for the "Heat" relay
-#define RELAY_PIN1 20 // "Fan" Relay trigger pin
+#define RELAY_PIN 10 // trigger pin for "Heat" relay
+#define RELAY_PIN0 9 // trigger pin for "Cooling" relay
+#define RELAY_PIN1 20 // trigger pin for "Fan" relay
 
 const char* ssid = "your_SSID";				 // Change to desired Wi-Fi SSID
 const char* password = "your_SSID_Password"; // Change to desired Wi-Fi Password
@@ -37,6 +49,7 @@ unsigned long lastDebounceTime = 0; // Momentary switches like to be bouncy
 unsigned long debounceDelay = 1000; // These variables will take care of that later
 
 bool heatingOn = false; // heating status, starts as false since RELAY_PIN typically starts LOW
+bool coolingOn = false; // cooling status
 bool fanisOn = false; // same as above but for.... the fan relay
 
 float celsiusToFahrenheit(float celsius) { //Converts Celsius to Fahrenheit
@@ -48,19 +61,35 @@ void displayTempSet(int tempSet) { // This function gets flashed briefly on the 
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 20);
+  display.setCursor(0, 20);
   display.print(F("Target: "));
   display.print(tempSet);
   display.display();
 }
 
+void displayFanStatus() {    // Gets flashed to the OLED whenever fanisOn changes
+	display.clearDisplay();
+	display.setTextSize(2);
+	display.setTextColor(SSD1306_WHITE);
+	display.setCursor(0, 20);
+	if (fanisOn) {
+		display.print(F("Fan: On"));
+	} else {
+		display.print(F("Fan: Off"));
+	}
+	display.display();
+}
+
 void setup() {
  // Serial.begin(115200);      // uncomment this if you need serial output for debugging
+	
   pinMode(RELAY_PIN, OUTPUT); 
-  pinMode(RELAY_PIN1, OUTPUT);
+  pinMode(RELAY_PIN1, OUTPUT); // Set Relay trigger pins as outputs, because they are.
   digitalWrite(RELAY_PIN, LOW); // Ensure the relay is initially off
   digitalWrite(RELAY_PIN1, LOW);
   pinMode(BUTTON_PIN0, INPUT_PULLUP); // Gotta use a pullup for buttons because otherwise
   pinMode(BUTTON_PIN1, INPUT_PULLUP); // Erwins Cats are going to get involved
+  pinMode(BUTTON_FAN, INPUT_PULLUP);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -81,7 +110,8 @@ void setup() {
 
   dht.begin();
 
-  // Serve a webpage to control features remotely
+  // Serve a webpage to control features remotely, 
+  // also added password protection so that you can access it outside of your home LAN
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
   if (!request->authenticate(http_username, http_password)) {
     return request->requestAuthentication();
@@ -173,7 +203,6 @@ void setup() {
     tempSet -= 1.0; // Decrease the target temperature by 1 degree
     request->send(200, "text/plain", "Decreased target temperature by 1 degree.");
   });
-
   server.on("/fanOn", HTTP_GET, [](AsyncWebServerRequest *request) {
 	  if (!request->authenticate(http_username, http_password)) {
     return request->requestAuthentication();
@@ -181,20 +210,24 @@ void setup() {
   digitalWrite(RELAY_PIN1, HIGH); // Turn on the fan relay
   fanisOn = true;
   request->send(200, "text/plain", "Fan is turned on.");
+  displayFanStatus(); // Gets called everytime fanisOn changes throughout the code
 });
 
   server.on("/fanOff", HTTP_GET, [](AsyncWebServerRequest *request) {
 	  if (!request->authenticate(http_username, http_password)) {
     return request->requestAuthentication();
   }
+	  
   digitalWrite(RELAY_PIN1, LOW); // Turn off the fan relay
   fanisOn = false;
   request->send(200, "text/plain", "Fan is turned off.");
+  displayFanStatus();
 });
   server.begin();
 
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN0), button0Pressed, FALLING); // Interrupt to detect button press
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN1), button1Pressed, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_FAN), buttonfanPressed, FALLING);
 }
 
 void button0Pressed() {
@@ -210,6 +243,19 @@ void button1Pressed() {
     lastDebounceTime = millis();
   }
 }
+void buttonfanPressed() {
+  if (millis() - lastDebounceTime > debounceDelay) {
+    if (fanisOn) {
+      digitalWrite(RELAY_PIN1, LOW); // Turn off the fan relay when the physical button is pressed and the fan is already on
+      fanisOn = false;				 
+    } else {
+      digitalWrite(RELAY_PIN1, HIGH); // Opposite of above
+      fanisOn = true;
+    }
+    lastDebounceTime = millis();
+	displayFanStatus();
+  }
+}
 
 void loop() {
   static float previousTempSet = tempSet; // Store the previous value of tempSet
@@ -223,9 +269,9 @@ void loop() {
 
   int rssi = WiFi.RSSI(); 								 // Comments are exhausting.... I think you can figure this one out.
   Serial.print("Wi-Fi Signal Strength (RSSI): ");
-  Serial.print(rssi);
+  Serial.print(rssi);                                    // Not sure why i didnt just output the function itself though
   Serial.println(" dBm");
-
+	
   // Check if any reads failed
   if (!isnan(temperatureC) && !isnan(humidity)) {  // Display the temperature in Fahrenheit on the OLED screen
     display.clearDisplay(); // Clear the OLED first in case anything was left behind
@@ -249,7 +295,7 @@ void loop() {
     display.display(); // Show that sweet sweet information on the OLED
 
     unsigned long lastFurnaceOffTime = 0; // Store the last time the furnace was turned off
-    unsigned long furnaceDelay = 120000;    // Delay before turning the furnace back on (60,000 ms = 60 seconds)
+    unsigned long furnaceDelay = 120000;    // Delay before turning the furnace back on (120,000 ms = 120 seconds)
 
     // Control the relay based on the target temperature (tempSet)
     if (temperatureF < (tempSet - 2)) {
@@ -267,7 +313,7 @@ void loop() {
       }
 
       // Check if tempSet has changed
-      if (temperatureF < (tempSet - 2) || tempSet != previousTempSet) { // Math is torture
+      if (temperatureF < (tempSet - 2) || tempSet != previousTempSet) { // logic  is torture
         // Print target temperature to the serial monitor
         Serial.print(F("Target Temperature: "));
         Serial.print(tempSet, 1);
